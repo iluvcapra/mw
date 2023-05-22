86101a04d94cc436c7e1fc5bc1f78d0441aa9b78
Jamie Hardt <jamiehardt@me.com>
2023-05-22 13:04:18 -0700

    Added a test stub

---
+++ test/test_app.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
