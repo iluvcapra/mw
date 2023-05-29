aba90e8a1b93a1810b5cdcfcdacb60fd079b657d
Jamie Hardt <jamiehardt@me.com>
2023-05-29 14:09:16 -0700

    Testing

--- test/test_command.py
+++ test/test_command.py
@@ -1,13 +1,17 @@
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock 
 from unittest import TestCase
 
 from mw import commands
+from mw.app import App
+from mw.display import Display
+from mw.stack import Stack
+
 
 class TestCommands(TestCase):
     def setUp(self) -> None:
-        self.mock_app = MagicMock()
-        self.mock_app.display = MagicMock()
-        self.mock_app.stack = MagicMock()
+        self.mock_app = MagicMock(spec=App)
+        self.mock_app.display = MagicMock(spec=Display)
+        self.mock_app.stack = MagicMock(spec=Stack)
         self.command_handler = commands.CommandHandler()
         return super().setUp()
 
@@ -16,14 +20,13 @@ class TestCommands(TestCase):
         self.mock_app.display.reset_mock()
         self.mock_app.stack.reset_mock()
         return super().tearDown()
-
-    def test_show(self):
-        self.command_handler.show(self.mock_app)
-        self.mock_app.display.print_head.assert_called()
     
     def test_license(self):
         self.command_handler.license(self.mock_app)
         self.mock_app.license.assert_called()
+    
+    def test_help(self):
+        self.command_handler.help(self.mock_app)
 
 
      
